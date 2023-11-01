d011bbf7efb69c5d47845d117290db60ed47c721
vitaly <dudkin231@mail.ru>
2023-11-01 10:19:42 +0300

    added views.py

--- schedule/views.py
+++ schedule/views.py
@@ -1,3 +1,51 @@
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticated
+
+from schedule.models import Habit
+from schedule.paginators import HabitPaginator
+from schedule.permissions import IsOwner, IsSuperuser
+from schedule.serializers import HabitSerializer
+
 
 # Create your views here.
+
+
+class HabitCreateListView(generics.ListCreateAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    pagination_class = HabitPaginator
+    permission_classes = [IsAuthenticated,]
+
+    def get_queryset(self):
+        """
+            Getting Lesson Objects based on User
+        """
+        queryset = super().get_queryset()
+        print(Habit.objects.filter(is_public=True))
+
+        if self.request.user.is_staff or self.request.user.is_superuser:
+            return queryset.all()
+        elif self.request.user:
+            return queryset.filter(user=self.request.user)
+        else:
+            return queryset.none()
+
+    def perform_create(self, serializer):
+        new_habit = serializer.save()
+        new_habit.user = self.request.user
+        new_habit.save()
+
+
+class HabitRetrieveUpdateDestroyAPIView(RetrieveUpdateDestroyAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.all()
+    permission_classes = [IsOwner | IsSuperuser]
+
+
+class PublicHabitsListView(generics.ListAPIView):
+    serializer_class = HabitSerializer
+    queryset = Habit.objects.filter(is_public=True)
+    permission_classes = [IsAuthenticated]
+
