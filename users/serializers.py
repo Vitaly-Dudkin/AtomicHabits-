b562c77f881a1204f9b187373a7959157b4731ac
vitaly <dudkin231@mail.ru>
2023-11-08 14:53:21 +0300

    fix password problem

--- users/serializers.py
+++ users/serializers.py
@@ -1,4 +1,4 @@
-from rest_framework import serializers
+from django.contrib.auth.hashers import make_password
 from rest_framework.serializers import ModelSerializer
 
 from users.models import User
@@ -9,3 +9,6 @@ class UserSerializer(ModelSerializer):
     class Meta:
         model = User
         fields = '__all__'
+
+    def validate_password(self, value):
+        return make_password(value)
